814863e305355489c84a61b98e9e811819896718
di.qin <di.qin@thoughtworks.com>
2020-10-21 17:57:28 +0800

    [qd]: add UT.

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,3 +1,31 @@
-test("add 1+1 is 2", () => {
-  expect(2).toBe(2);
+import { getEducationsByUserId, getUserById } from "../http";
+
+const baseURL = "http://localhost:8080/";
+
+describe("get user by user id", () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ name: "qindi", description: "TWer" }),
+      })
+    );
+  });
+
+  test("should get user info", async () => {
+    await expect(getUserById(111)).resolves.toEqual({
+      name: "qindi",
+      description: "TWer",
+    });
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(`${baseURL}/users/111`);
+  });
+
+  test("should get educations info", async () => {
+    await expect(getEducationsByUserId(111)).resolves.toEqual({
+      name: "qindi",
+      description: "TWer",
+    });
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(`${baseURL}/users/111/educations`);
+  });
 });
