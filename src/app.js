cbbd7018aba5a3d1b06eec6df1d7122abbe5de40
di.qin <di.qin@thoughtworks.com>
2020-10-21 16:45:41 +0800

    [qd]: add about me and education component render.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 /* eslint-env jquery */
 import "./style/index.scss";
-import { getUserById } from "./http";
+import { getUserById, getEducationsByUserId } from "./http";
 
 const { pathname } = window.location;
 const [, preString, userId] = pathname.split("/");
@@ -13,9 +13,35 @@ const renderHeader = (name, age, avatar) => {
   );
 };
 
+const renderAboutMe = (description) => {
+  $(".about-title").html("ABOUT ME");
+  $(".about-content").html(description);
+};
+
+const renderEducation = (userEducationsInfo) => {
+  let educationLiEle = "";
+  userEducationsInfo.forEach(
+    // eslint-disable-next-line no-return-assign
+    (cur) =>
+      (educationLiEle += `
+    <li>
+        <span>${cur.year}</span>
+        <div>
+            <span>${cur.title}</span>
+            <p>${cur.description}</p>
+        </div>
+    </li>
+  `)
+  );
+  $(".education-content").append(educationLiEle);
+};
+
 const renderPage = async (id) => {
-  const { name, age, avatar } = await getUserById(id);
+  const { name, age, avatar, description } = await getUserById(id);
+  const userEducationsInfo = await getEducationsByUserId(id);
   renderHeader(name, age, avatar);
+  renderAboutMe(description);
+  renderEducation(userEducationsInfo);
 };
 
 if (preString === "users") {
